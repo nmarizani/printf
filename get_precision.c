5e8edbf72d1a947dbe813725bcae14f0223ab9af
cipherKingg <ichbineinbavarian@gmai.com>
2023-09-18 23:49:30 -0700

    update_cipherkingg

---
+++ get_precision.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * get_precision - Calculates the precision for printing
+ * @format: Formatted string in which the
+ * argument is to be printed
+ * @i: List of arguments to be printed
+ * @list: list of arguments
+ *
+ * Return: Precision
+ */
+int get_precision(const char *format, int *i, va_list list)
+{
+	int curr_i = *i + 1;
+	int prec = -1;
+
+	if (format[curr_i] != '.')
+		return (prec);
+
+	prec = 0;
+
+	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+	{
+		if (is_digit(format[curr_i]))
+		{
+			prec *= 10;
+			prec += format[curr_i] - '0';
+		}
+		else if (format[curr_i] == '*')
+		{
+			curr_i++;
+			prec = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
+
+	*i = curr_i - 1;
+
+	return (prec);
+}
